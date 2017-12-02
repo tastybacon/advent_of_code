109ab9a729a3bcd72d0779cb177cb373dbe3497a
tastybacon <tastycuredpork@gmail.com>
2017-12-02 17:59:52 -0500

    Day 2: Part 2

--- 2017/02/corruption_checksum.erl
+++ 2017/02/corruption_checksum.erl
@@ -1,6 +1,7 @@
 -module(corruption_checksum).
--compile(export_all).
+-export([file_checksum/1, checksum/1, file_div_checksum/1, div_checksum/1]).
 
+% Part 1
 checksum(Spreadsheet) ->
   checksum(Spreadsheet, 0).
 
@@ -28,11 +29,45 @@ row_checksum([], Max, Min) ->
 file_checksum(Filename) ->
   checksum(read_spreadsheet(Filename)).
 
+% Part 2
+file_div_checksum(Filename) ->
+  div_checksum(read_spreadsheet(Filename)).
+
+div_checksum(Spreadsheet) ->
+  div_checksum(Spreadsheet, 0).
+
+div_checksum([], Acc) ->
+  Acc;
+div_checksum([H | T], Acc) ->
+  div_checksum(T, Acc + div_row_checksum(H)).
+
+div_row_checksum([]) ->
+  0;
+div_row_checksum(Row) ->
+  div_row_checksum(Row, Row).
+
+div_row_checksum([Current | Rest], FullRow) ->
+  case first_divisible(Current, FullRow) of
+    none -> div_row_checksum(Rest, FullRow);
+    [A, B] -> A div B
+  end.
+
+first_divisible(_, []) ->
+  none;
+first_divisible(X, [X | T]) ->
+  first_divisible(X, T);
+first_divisible(X, [H | _]) when X rem H =:= 0->
+  [X, H];
+first_divisible(X, [_ | T]) ->
+  first_divisible(X, T).
+
+% Util
+
 read_spreadsheet(Filename) ->
 	{ok, Data} = file:read_file(Filename),
   Lines = binary:split(Data, [<<"\n">>], [global]),
   lists:map(fun parse_row/1, Lines).
-
+  
 parse_row(<<>>) ->
   [];
 parse_row(Row) ->
