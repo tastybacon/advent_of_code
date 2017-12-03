3fb2e1af335b9768a1b848a675534ca9afd4bb3e
tastybacon <tastycuredpork@gmail.com>
2017-12-03 16:28:28 -0500

    Day 3: Spiral Memory (Part 2)

--- 2017/03/spiral_memory.erl
+++ 2017/03/spiral_memory.erl
@@ -1,5 +1,5 @@
 -module(spiral_memory).
--export([manhattan_distance/1, find_point/1, shell/1, test/0]).
+-export([manhattan_distance/1, find_point/1, shell/1, test/0, stress_test/1]).
 
 origin() ->
   {point, 0, 0}.
@@ -49,8 +49,6 @@ next_point({point, X, Y}, _) when Y == abs(X) ->
 next_point({point, _, _} = P, {point, _, _} = DP) ->
   {add_point(P, DP), DP}.
 
-  
-  
 test() ->
   test_find_point(1, {point, 0, 0}),
   test_find_point(2, {point, 1, 0}),
@@ -68,3 +66,40 @@ test() ->
 test_find_point(Input, Expected) ->
   Result = find_point(Input),
   io:format("Input: ~w | Expected: ~w | Got: ~w | Result ~w~n", [Input, Expected, Result, Result =:= Expected]).
+
+% Part 2
+
+sum_surrounding({point, X, Y}, Array) ->
+  get_value(X + 1, Y, Array) +
+  get_value(X + 1, Y + 1, Array) +
+  get_value(X, Y + 1, Array) +
+  get_value(X - 1, Y + 1, Array) +
+  get_value(X - 1, Y, Array) +
+  get_value(X - 1, Y - 1, Array) +
+  get_value(X, Y - 1, Array) +
+  get_value(X + 1, Y - 1, Array).
+
+get_value({point, X, Y}, Array) ->
+  get_value(X, Y, Array).
+get_value(X, Y, Array) ->
+  array:get(X * 1000 + Y, Array).
+
+set_value({point, X, Y}, Value, Array) ->
+  set_value(X, Y, Value, Array).
+set_value(X, Y, Value, Array) ->
+  array:set(X * 1000 + Y, Value, Array).
+
+stress_test(Target) ->
+  stress_test(Target, array:new({default, 0}), origin(), none, {point, 500, 500}).
+stress_test(Target, Array, {point, _, _} = Point, State, Center) ->
+  {NextPoint, NextState} = next_point(Point, State),
+  CorrectedPoint = add_point(Point, Center),
+  NextValue = case Point of
+                {point, 0, 0} -> 1;
+                _ -> sum_surrounding(CorrectedPoint, Array)
+              end,
+  case Target < NextValue of
+    true -> {NextValue, Point};
+    false -> NewArr = set_value(CorrectedPoint, NextValue, Array),
+             stress_test(Target, NewArr, NextPoint, NextState, Center)
+  end.
