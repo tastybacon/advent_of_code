9431d73f1e724b6720a8aca10b7c9a146e061939
tastybacon <tastycuredpork@gmail.com>
2017-12-05 21:53:53 -0500

    Day 5: A Maze of Twisty Trampolines, All Alike (Part 2)

--- 2017/05/twisty_trampolines.erl
+++ 2017/05/twisty_trampolines.erl
@@ -1,19 +1,31 @@
 -module(twisty_trampolines).
--compile(export_all).
+-export(
+   [
+    count_steps/1,
+    count_steps/2,
+    read_jump_list/1,
+    part_1_offset/1,
+    part_2_offset/1
+   ]
+  ).
 
 count_steps(List) ->
+  count_steps(List, fun part_1_offset/1).
+count_steps(List, OffsetFunction) ->
   Array = array:from_list(List),
-  step(0, 0, Array).
+  step(0, 0, Array, OffsetFunction).
 
-step(Position, Acc, Array) when Position >= 0 ->
+step(Position, Acc, Array, OffsetFunction) when Position >= 0 ->
   CurrentOffset = array:get(Position, Array),
   NewPosition = Position + CurrentOffset,
   StepCount = Acc + 1,
   case NewPosition < array:size(Array) of
-    true -> step(NewPosition, StepCount, array:set(Position, CurrentOffset + 1, Array));
+    true ->
+      NewOffset = OffsetFunction(CurrentOffset),
+      step(NewPosition, StepCount, array:set(Position, NewOffset, Array), OffsetFunction);
     false -> StepCount
   end;
-step(_, Acc, _) ->
+step(_, Acc, _, _) ->
   Acc + 1.
 
 read_jump_list(Filename) ->
@@ -28,3 +40,11 @@ read_jump_list(Filename) ->
 
 read_list_item(RawItem) ->
   list_to_integer(binary_to_list(RawItem)).
+
+part_1_offset(CurrentOffset) ->
+  CurrentOffset + 1.
+
+part_2_offset(CurrentOffset) when CurrentOffset >= 3 ->
+  CurrentOffset - 1;
+part_2_offset(CurrentOffset) ->
+  CurrentOffset + 1.
