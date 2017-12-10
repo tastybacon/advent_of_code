933da29dda7210124554cefb5e33638cbbc1d879
tastybacon <tastycuredpork@gmail.com>
2017-12-10 17:35:33 -0500

    Day 10: Knot Hash (part 2)

--- 2017/10/knot_hash.erl
+++ 2017/10/knot_hash.erl
@@ -1,18 +1,52 @@
 -module(knot_hash).
--compile(export_all).
+-export([compute_hash_round/2, hash/0, hash/3]).
 
-compute_hash(Lengths, HashLength) ->
-  compute_hash(Lengths, lists:seq(0, HashLength - 1), 0, 0).
+dense_hash(SparseHash) ->
+  dense_hash(SparseHash, []).
 
-compute_hash([], HashList, _, _) ->
-  {FirstTwo, _} = lists:split(2, HashList),
-  io:format("Reached the end of Lengths with ~w~n", [HashList]),
-  lists:foldl(fun(X, Prod) -> X * Prod end, 1, FirstTwo);
-compute_hash([H|T], HashList, CurrentPosition, SkipLength) when H =< length(HashList) ->
+dense_hash([], Acc) ->
+  list_to_binary(
+    lists:flatmap(
+      fun(X) -> io_lib:format("~2.16.0B", [X]) end,
+      lists:reverse(Acc)
+     )
+   );
+dense_hash(SparseHash, DenseAcc) ->
+  {Current, Rest} = lists:split(16, SparseHash),
+  Densified = lists:foldl(fun(X, Acc) -> X bxor Acc end, 0, Current),
+  dense_hash(Rest, [Densified | DenseAcc]).
+
+hash() ->
+  hash("187,254,0,81,169,219,1,190,19,102,255,56,46,32,2,216", 256, 64).
+
+hash(Input, HashLength, Rounds) ->
+  hash(Input ++ [17, 31, 73, 47, 23], lists:seq(0, HashLength - 1), Rounds, 0, 0).
+
+hash(_, SparseHash, 0, _, _) ->
+  dense_hash(SparseHash);
+hash(Input, HashRound, RemainingRounds, CurrentPosition, SkipLength) ->
+  {NewHashRound, NewPosition, NewSkipLength} = compute_hash_round(Input, HashRound, CurrentPosition, SkipLength),
+  %io:format("Input: ~w~n", [Input]),
+  io:format("Rounds: ~w, Position: (~w -> ~w), SkipLength: (~w -> ~w)~n",
+            [RemainingRounds, CurrentPosition, NewPosition, SkipLength, NewSkipLength]),
+  hash(
+    Input,
+    NewHashRound,
+    RemainingRounds - 1,
+    NewPosition,
+    NewSkipLength
+   ).
+
+compute_hash_round(Lengths, HashLength) ->
+  compute_hash_round(Lengths, lists:seq(0, HashLength - 1), 0, 0).
+
+compute_hash_round([], HashList, CurrentPosition, SkipLength) ->
+  {HashList, CurrentPosition, SkipLength};
+compute_hash_round([H|T], HashList, CurrentPosition, SkipLength) when H =< length(HashList) ->
   NewHashList  = reverse_sublist(HashList, CurrentPosition, H),
   NewPosition = (H + CurrentPosition + SkipLength) rem length(HashList),
-  % io:format("Position ~w -> ~w~n", [CurrentPosition, NewPosition]),
-  compute_hash(T, NewHashList, NewPosition, SkipLength + 1).
+  %io:format("Position: ~w -> ~w~n", [CurrentPosition, NewPosition]),
+  compute_hash_round(T, NewHashList, NewPosition, SkipLength + 1).
 
 reverse_sublist(List, Start, Len) ->
   StartIndex = Start,
@@ -21,15 +55,11 @@ reverse_sublist(List, Start, Len) ->
     X when X < 0 ->
       {NewFirst, Rest} = lists:split(Start, List),
       {Middle, NewLast} = lists:split(Len, Rest),
-      NewMiddle = lists:reverse(Middle),
-      io:format("Reversed Portion: ~w~n", [NewMiddle]);
+      NewMiddle = lists:reverse(Middle);
     OverflowLength ->
       {First, Rest} = lists:split(OverflowLength, List),
       {NewMiddle, Last} = lists:split(StartIndex - OverflowLength, Rest),
       Reversed = lists:reverse(Last ++ First),
-      io:format("Reversed Portion: ~w~n", [Reversed]),
       {NewLast, NewFirst} = lists:split(Len - OverflowLength, Reversed)
   end,
-  io:format("StartIndex: ~w, First: ~w, Middle: ~w, Last: ~w, Combined: ~w~n",
-            [StartIndex, NewFirst, NewMiddle, NewLast, NewFirst ++ NewMiddle ++ NewLast]),
   NewFirst ++ NewMiddle ++ NewLast.
