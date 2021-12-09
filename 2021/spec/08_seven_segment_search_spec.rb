97e7e38557d93ed6f03588503d74ee06f3c3221f
tastybacon <tastybaconbit@gmail.com>
2021-12-09 05:04:26 +0000

    Whoops, those were placeholder test names

--- 2021/spec/08_seven_segment_search_spec.rb
+++ 2021/spec/08_seven_segment_search_spec.rb
@@ -4,19 +4,19 @@ require 'seven_segment_search'
 
 describe 'Day 08: Seven Segment Search' do
   context 'Part 1' do
-    it 'does the thing' do
+    it 'finds the number of unique digits in the test input' do
       input = SevenSegmentSearch.parse_input(File.read('resources/08/test_input.txt'))
       expect(SevenSegmentSearch.count_unique_digits(input)).to eq 26
     end
 
-    it 'does the thing' do
+    it 'finds the number of unique digits in the input' do
       input = SevenSegmentSearch.parse_input(File.read('resources/08/input.txt'))
       expect(SevenSegmentSearch.count_unique_digits(input)).to eq 412
     end
   end
 
   context 'Part 2' do
-    it 'does the thing' do
+    it 'decodes the signal of the test input' do
       input = SevenSegmentSearch.parse_input(File.read('resources/08/test_input.txt'))
       result = input.sum do |line|
         SevenSegmentSearch.decode_signal(line)
@@ -24,7 +24,7 @@ describe 'Day 08: Seven Segment Search' do
       expect(result).to eq 61_229
     end
 
-    it 'does the thing' do
+    it 'decodes the signal of the test input' do
       input = SevenSegmentSearch.parse_input(File.read('resources/08/input.txt'))
       result = input.sum do |line|
         SevenSegmentSearch.decode_signal(line)
