e226377e556df2090b35e6486b96f4bf13e487f3
tastybacon <tastybaconbit@gmail.com>
2021-12-10 05:38:49 +0000

    Optimize 2021 Day 07 a bit

--- 2021/lib/crab_orchestration.rb
+++ 2021/lib/crab_orchestration.rb
@@ -19,16 +19,37 @@ module CrabOrchestration
     end
   end
 
+  def self.fuel_usage_multiple(position_counts, destination, fuel_consumption_model)
+    position_counts.sum do |position, count|
+      fuel_usage(position, destination, fuel_consumption_model) * count
+    end
+  end
+
   def self.determine_most_fuel_efficient_position(crab_positions, fuel_consumption_model)
-    min_position, max_position = crab_positions.minmax
+    position_counts = crab_positions.tally
     last_fuel_usage = nil
-    last_position = min_position
-    (min_position..max_position).each do |destination|
-      current_fuel_usage = crab_positions.sum { |p| fuel_usage(p, destination, fuel_consumption_model) }
+    last_position = nil
+    iterate_positions(crab_positions, position_counts, fuel_consumption_model).each do |destination|
+      current_fuel_usage = fuel_usage_multiple(position_counts, destination, fuel_consumption_model)
       return [last_position, last_fuel_usage] if !last_fuel_usage.nil? && current_fuel_usage >= last_fuel_usage
 
       last_position = destination
       last_fuel_usage = current_fuel_usage
+    end
+  end
+
+  # Iterates over different values in a semi-smart way. First, we guess that
+  # the most efficient position will be somewhere towards the middle and so
+  # we start at the average. Then we determine the direction of iteration based
+  # on two values.
+  def self.iterate_positions(crab_positions, position_counts, fuel_consumption_model)
+    guess = position_counts.sum { |a, b| a * b } / crab_positions.length
+    guess_fuel = position_counts.sum { |p, c| fuel_usage(p, guess, fuel_consumption_model) * c }
+    previous_fuel = position_counts.sum { |p, c| fuel_usage(p, guess - 1, fuel_consumption_model) * c }
+    if guess_fuel > previous_fuel
+      (guess - 1..).step(-1)
+    else
+      (guess..)
     end
   end
 end
